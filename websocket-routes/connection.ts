9e154ebab6d45b720b6b7058a6625abef800994d
Kamil <kamil.pieczyk@gmail.com>
2020-12-06 12:30:32 +0000

    FIX WEBSOCKET ROUTES

--- websocket-routes/connection.ts
+++ websocket-routes/connection.ts
@@ -1,8 +1,11 @@
 import { Socket } from 'socket.io';
+import websocketRoutes from './index';
 
 const connection = (socket: Socket) => {
   console.info('websocket connection ok');
   socket.emit('connection-status', 'connected');
+  socket.on('get-orders', websocketRoutes.orders);
+  socket.on('new-order', websocketRoutes.orders);
 };
 
 export default connection;
