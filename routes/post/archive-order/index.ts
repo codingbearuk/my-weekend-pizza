7418d2989e5b88e459c38be82e30f11f3c950270
Kamil <kamil.pieczyk@gmail.com>
2020-12-13 13:05:24 +0000

    FIX ARCHIVE ORDER SAVE TO DATABASE;

--- routes/post/archive-order/index.ts
+++ routes/post/archive-order/index.ts
@@ -1,14 +1,27 @@
 import OrderModel from '../../../models/order.model';
 import OrderArchiveModel from '../../../models/order-archive.model';
 import Route from '../../route.type';
-import orderArchiveModel from '../../../models/order-archive.model';
 
 const archiveOrder: Route = async (req, res) => {
   const id: string = req.body.id;
   try {
     const removedOrder = await OrderModel.findOneAndDelete({ order_id: id });
     if (!removedOrder) throw new Error('could not delete this item');
-    const orderToArchive = new OrderArchiveModel(removedOrder);
+    const document = {
+      order_id: removedOrder.order_id,
+      cart: removedOrder.cart,
+      address: {
+        city: removedOrder.address.city,
+        postcode: removedOrder.address.postcode,
+        address: removedOrder.address.address,
+        phone: removedOrder.address.phone,
+        deliveryDate: removedOrder.address.deliveryDate,
+      },
+      totalPrice: removedOrder.totalPrice,
+      paymentSuccess: removedOrder.paymentSuccess,
+      status: removedOrder.status,
+    };
+    const orderToArchive = new OrderArchiveModel(document);
     const archive = await orderToArchive.save();
     if (!archive) throw new Error('could not archive this item');
     res.status(200).json({
