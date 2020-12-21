5986215b90471b45db74310af81897f45bb44577
Kamil <kamil.pieczyk@gmail.com>
2020-12-21 16:09:16 +0000

    FIX PRODUCTION MODE PROBLEM

--- routes/get/get-all-images/index.ts
+++ routes/get/get-all-images/index.ts
@@ -3,8 +3,27 @@ import fs from 'fs';
 import { join } from 'path';
 
 const getAllImages: Route = (req, res) => {
-  const pathToPizzaImages: string = join(__dirname, '..', '..', '..', 'public', 'images', 'pizzas');
-  const pathToSauceImages: string = join(__dirname, '..', '..', '..', 'public', 'images', 'sauces');
+  const prod: boolean = process.env.NODE_ENV === 'production';
+  const pathToPizzaImages: string = join(
+    __dirname,
+    prod ? '..' : '',
+    '..',
+    '..',
+    '..',
+    'public',
+    'images',
+    'pizzas'
+  );
+  const pathToSauceImages: string = join(
+    __dirname,
+    prod ? '..' : '',
+    '..',
+    '..',
+    '..',
+    'public',
+    'images',
+    'sauces'
+  );
   const pizzas: Array<string> = fs.readdirSync(pathToPizzaImages);
   const sauces: Array<string> = fs.readdirSync(pathToSauceImages);
   res.status(200).json({
