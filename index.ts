a3c731a3bac6ad6c49ac02c8275564ee9b826464
Kamil Pieczyk <kamil.pieczyk@gmail.com>
2021-01-31 12:13:54 +0000

    add allow acces origins headers

--- index.ts
+++ index.ts
@@ -37,6 +37,15 @@ class Server {
         responseOnLimit: 'File size limit has been reached',
       })
     );
+
+    server.use(function (req, res, next) {
+      res.setHeader('Access-Control-Allow-Origin', '*');
+      res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+      res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+      // res.setHeader('Access-Control-Allow-Credentials', true);
+      next();
+    });
+
     this.database();
     this.get();
     this.post();
